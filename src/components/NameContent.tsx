57c9241e1574da6c2757221c56949ca84cf4a135
Razvan Filea <razvan.filea@gmail.com>
2022-11-09 15:21:18 +0200

    Finish app bar

---
+++ src/components/NameContent.tsx
@@ -0,0 +1,7 @@
+export const NameContent = () => {
+    return <div className="h-[90vh]">
+        <h1 className="center whitespace-nowrap text-white font-serif text-4xl">
+            Hello, I am <div className="inline">Răzvan.</div>
+        </h1>
+    </div>
+}
--- src/components/NavBarButtons.tsx
+++ src/components/NavBarButtons.tsx
@@ -3,7 +3,7 @@ interface NavBarButtonParams {
 }
 
 export const NavBarButton = ({ content, link }: NavBarButtonParams) => {
-    return <a href={link} className="py-3 px-5 mx-1 rounded-lg select-none hover:bg-primary text-primary font-bold uppercase
+    return <a href={link} className="py-3 px-5 md:mx-1 rounded-lg select-none hover:bg-primary text-primary font-bold uppercase
         hover:bg-opacity-30 hover:underline decoration-wavy decoration-3 underline-offset-8
         transition-all duration-200 ease-linear">
         {content}
@@ -15,7 +15,7 @@ interface SocialMediaButton {
 }
 
 export const SocialMediaButton = ({ link, children }: SocialMediaButton) => {
-    return <a href={link} rel="noopener" target="_blank" className="py-3 px-5 mx-1 w-6 h-6 rounded-lg bg-primary bg-opacity-20
+    return <a href={link} rel="noopener" target="_blank" className="py-3 px-5 mx-0.5 md:mx-1 w-6 h-6 rounded-lg bg-primary bg-opacity-20
         hover:bg-opacity-50
         transition-all duration-200 ease-linear">
         {children}
