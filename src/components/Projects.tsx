922ab3beec475a343b54a20fd7a14a22d61887e3
Razvan Filea <razvan.filea@gmail.com>
2022-11-10 12:06:08 +0200

    Finish Projects basic layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,7 @@
-// import {onCleanup, createSignal, Component} from "solid-js"
 import 'solid-js';
+import { createSignal, Component, createEffect, For } from "solid-js"
+import Styles from '../styles/Projects.module.css'
+
 class CardData {
     readonly title: string;
     readonly summary: string;
@@ -21,32 +23,36 @@ const data: CardData[] = [
 
 function Card(cardData: CardData) {
     return (
-    <div className="card" >
-        <div className="card-content">
-            <p className="text-slate-200">{cardData.title}</p>
+        <div class={Styles.card} >
+            <div class={Styles.cardContent}>
+                <p class="text-slate-200">{cardData.title}</p>
+            </div>
         </div>
-    </div>
     )
 }
 
 export const Projects = () => {
+    let ref: any;
+    const [pos, setPos] = createSignal({ x: 0, y: 0 });
 
-    /* let ref = undefined;
-    const pos = useMousePosition();
-    const [el, setEl] = createSignal(ref);
-    const relative = createPositionToElement(ref, () => pos);
+    function handleMouseMove(event: { clientX: any; clientY: any; }) {
+        setPos({
+            x: event.clientX,
+            y: event.clientY
+        });
+    }
 
     createEffect(() => {
-        if (!ref.current) return
+        if (!ref) return
 
-        for (const child of (ref.current.children as HTMLCollection)) {
+        for (const child of (ref.children as HTMLCollection)) {
             const rect = child.getBoundingClientRect();
-            const xPx = `${x - rect.left}px`
-            const yPx = `${y - rect.top}px`
-            
+            const xPx = `${pos().x - rect.left}px`
+            const yPx = `${pos().y - rect.top}px`
+
             child.style.setProperty(
                 "--mouse-x",
-                x !== 0 ? xPx : null
+                pos().x !== 0 ? xPx : null
             );
 
             child.style.setProperty(
@@ -54,13 +60,11 @@ export const Projects = () => {
                 yPx
             );
         }
-    }) */
+    })
 
-    return <div >
-        <div id="cards" className="grid grid-row-3 grid-flow-col gap-2 px-6" >
-            {data.map((cardData) => {
-                return <>{Card(cardData)}</>
-            })}
-        </div>
+    return <div id={Styles.cards} class="grid grid-row-3 grid-flow-col gap-2 px-6" ref={ref} onMouseMove={handleMouseMove}>
+        <For each={data}>
+            {(item) => Card(item)}
+        </For>
     </div>
 };
